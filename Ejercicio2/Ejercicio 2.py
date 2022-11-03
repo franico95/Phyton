5c877c691cb36e8c69876008886fa13fda9f3e25
franico95 <85717501+franico95@users.noreply.github.com>
2022-11-03 11:42:40 -0300

    Add files via upload

---
+++ Ejercicio2/Ejercicio 2.py
@@ -0,0 +1,8 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a= "Hola mundo!"
+>>> print (a);
+Hola mundo!
+>>> a= "Buenos dias mundo!"
+>>> print (a)
+Buenos dias mundo!
